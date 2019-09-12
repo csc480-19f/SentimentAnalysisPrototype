47c5bbefa96cae70027d9a8e59b3a2c4b71d1b28
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2019-09-11 21:39:56 -0400

    Add files via upload
    Added citations and links.

--- SentimentPrototype.py
+++ SentimentPrototype.py
@@ -3,6 +3,14 @@ from nltk.tokenize import sent_tokenize
 # import time
 import sys
 
+"""
+If you use the VADER sentiment analysis tools, please cite:
+
+Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for
+Sentiment Analysis of Social Media Text. Eighth International Conference on
+Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
+"""
+
 # time.time() returns a floating point value, where 1.0 is one second.
 # begin = time.time()
 
